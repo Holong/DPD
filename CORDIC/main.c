5ab3b5cce38860969d2925faf9913f7e1a22edc1
JunYoung Moon <kit22978@hanmail.net>
2014-03-13 15:17:15 +0900

    make CORDIC angle table

--- CORDIC/main.c
+++ CORDIC/main.c
@@ -1,7 +1,13 @@
-/*
- * main.c
- */
+#include <stdio.h>
+
+#include "lookup.h"
+
 int main(void) {
-	
+	int i;
+
+	for(i = 0; i < TABLE_LENGTH; i++)
+	{
+		printf("%d, %lf, %lf\n", rotation_table[i].index, rotation_table[i].tangent, rotation_table[i].angle);
+	}
 	return 0;
 }
