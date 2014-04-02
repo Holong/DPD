8028d8267dad68092f48b6cdaa98aa4d9e77bd6d
JunYoung Moon <kit22978@hanmail.net>
2014-04-02 14:32:05 +0900

    Add dpd.c

---
+++ dpd.c
@@ -0,0 +1,46 @@
+#include "CORDIC/fixed.h"
+#include "CORDIC/cordic.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define INPUT_LEN		100
+#define OUTPUT_LEN		INPUT_LEN
+#define PRECISION		13
+
+extern double I_phase[INPUT_LEN];
+extern double Q_phase[INPUT_LEN];
+
+void cal_dpd(double amp, double phase, double *ret_amp, double *ret_phase)
+{
+}
+	
+
+void dpd(double I, double Q, double* ret_I, double* ret_Q)
+{
+	double before_DPD_amp;
+	double before_DPD_phase;
+
+	double after_DPD_amp;
+	double after_DPD_phase;
+
+	before_DPD_amp = CORDIC_sqrt_float(I, Q, PRECISION);
+	before_DPD_phase = CORDIC_atan_float(I, Q, PRECISION);
+
+	cal_dpd(before_DPD_amp, before_DPD_phase, &after_DPD_amp, &after_DPD_phase);
+
+	*ret_I = before_DPD_amp * CORDIC_cos_float(before_DPD_phase, PRECISION);
+	*ret_Q = before_DPD_amp * CORDIC_sin_float(before_DPD_phase, PRECISION);
+}
+
+int main(void)
+{
+	double ret_I_phase[OUTPUT_LEN];
+	double ret_Q_phase[OUTPUT_LNE];
+
+	for(i = 0; i < INPUT_LEN; i++) {
+		dpd(I_phase[i], Q_phase[i], &ret_I_phase[i], &ret_Q_phase[i]);
+	}
+	
+	return 0;
+}
